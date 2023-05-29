bc025af5a59476d631b377b685bb0744a6459c4a
dabuuraaa <ryotugboat@gmail.com>
2023-05-29 08:51:57 +0700

    add fundraisers

--- truffle/contracts/FundraiserFactory.sol
+++ truffle/contracts/FundraiserFactory.sol
@@ -4,6 +4,7 @@ pragma solidity >0.4.23 <0.7.0;
 import "./Fundraiser.sol";
 
 contract FundraiserFactory {
+  uint256 constant maxLimit = 20;
   Fundraiser[] private _fundraisers;
 
   event FundraiserCreated(Fundraiser indexed fundraiser, address indexed owner);
@@ -31,5 +32,20 @@ contract FundraiserFactory {
 
   function fundraisersCount() public view returns(uint256) {
     return _fundraisers.length;
+  }
+
+  function fundraisers(uint256 limit, uint256 offset) public view returns(Fundraiser[] memory coll) {
+    require(offset <= fundraisersCount(), "offset out of bounds");
+
+    uint256 size = fundraisersCount() - offset;
+    size = size < limit ? size : limit;
+    size = size < maxLimit ? size : maxLimit;
+    coll = new Fundraiser[](size);
+
+    for(uint256 i = 0; i < size; i++) {
+      coll[i] = _fundraisers[offset + i];
+    }
+
+    return coll;
   }
 }
